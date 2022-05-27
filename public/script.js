30834175ed2221ec13627a902278fdfea5e45e09
Josh <joshmwood95@gmail.com>
2022-05-27 02:33:58 -0400

    add buttons for filtering albums by genre

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 let albums = [];
+let filters = [];
 async function getAlbums() {
     const response = await fetch('/albums');
     console.log(response);
@@ -18,7 +19,6 @@ async function renderAlbums(method = sortByArtistTitle) {
     let container = document.getElementById("albumListContainer");
     container.innerHTML = "";
 
-
     // render each row
     let albumList = document.createElement("ol");
     // albumData = sortByScore(albumData);
@@ -26,59 +26,63 @@ async function renderAlbums(method = sortByArtistTitle) {
 
     for (let i = 0; i < albumData.length; i++) {
 
-        let albumDiv = document.createElement("li")
-        albumDiv.classList.add("album");
+
+        // check to see if the album has been filtered out
+        if (filters.indexOf(albumData[i][2]) == -1) {
+
+            let albumDiv = document.createElement("li")
+            albumDiv.classList.add("album");
 
 
-        // album cover
-        let img = document.createElement("img");
-        img.src = `/img/${albumData[i][4]}`;
-        img.classList.add("album--image")
-        albumDiv.appendChild(img);
+            // album cover
+            let img = document.createElement("img");
+            img.src = `/img/${albumData[i][4]}`;
+            img.classList.add("album--image")
+            albumDiv.appendChild(img);
 
-        let albumInfo = document.createElement("div");
-        albumInfo.classList.add("album--info");
+            let albumInfo = document.createElement("div");
+            albumInfo.classList.add("album--info");
 
-        // album title
-        let albumTitle = document.createElement("h1");
-        albumTitle.classList.add("album--title")
-        albumTitle.textContent = albumData[i][0];
-        albumInfo.appendChild(albumTitle);
+            // album title
+            let albumTitle = document.createElement("h1");
+            albumTitle.classList.add("album--title")
+            albumTitle.textContent = albumData[i][0];
+            albumInfo.appendChild(albumTitle);
 
-        // artist title
-        let artistTitle = document.createElement("h2");
-        artistTitle.classList.add("album--artist-title");
-        artistTitle.textContent = albumData[i][1];
-        albumInfo.appendChild(artistTitle);
-        albumDiv.appendChild(albumInfo);
+            // artist title
+            let artistTitle = document.createElement("h2");
+            artistTitle.classList.add("album--artist-title");
+            artistTitle.textContent = albumData[i][1];
+            albumInfo.appendChild(artistTitle);
+            albumDiv.appendChild(albumInfo);
 
-        // genre(s)
-        let albumGenre = document.createElement("div")
-        albumGenre.classList.add("genre");
-        let span = document.createElement("span");
-        span.textContent = albumData[i][2];
-        albumGenre.appendChild(span);
-        albumInfo.appendChild(albumGenre);
+            // genre(s)
+            let albumGenre = document.createElement("div")
+            albumGenre.classList.add("genre");
+            let span = document.createElement("span");
+            span.textContent = albumData[i][2];
+            albumGenre.appendChild(span);
+            albumInfo.appendChild(albumGenre);
 
-        // review
-        let reviewSentence = document.createElement("p");
-        reviewSentence.classList.add("album--review")
-        if (albumData[i][5]) {
-            reviewSentence.textContent = `"${albumData[i][5]}"`
-            albumInfo.appendChild(reviewSentence);
+            // review
+            let reviewSentence = document.createElement("p");
+            reviewSentence.classList.add("album--review")
+            if (albumData[i][5]) {
+                reviewSentence.textContent = `"${albumData[i][5]}"`
+                albumInfo.appendChild(reviewSentence);
+            }
+
+            //score
+            albumScore = document.createElement("div");
+            albumScore.classList.add("score");
+            let div = document.createElement("div");
+            div.textContent = `${albumData[i][3]}`
+            albumScore.appendChild(div);
+            albumDiv.appendChild(albumScore);
+
+            albumList.appendChild(albumDiv);
         }
-
-        //score
-        albumScore = document.createElement("div");
-        albumScore.classList.add("score");
-        let div = document.createElement("div");
-        div.textContent = `${albumData[i][3]}`
-        albumScore.appendChild(div);
-        albumDiv.appendChild(albumScore);
-
-        albumList.appendChild(albumDiv);
     }
-
 
     container.appendChild(albumList);
 }
@@ -101,6 +105,7 @@ function sortByArtistTitle(albumData) {
         let artistB = b[1].toUpperCase();
 
         // check if it begins with the word "The", and ignore it.
+        // i.e 'The Beatles' becomes 'Beatles, The'
         if (artistA.startsWith("THE ")) {
             artistA = artistA.slice(4);
         }
@@ -152,10 +157,35 @@ function sortByAlbumButton() {
     renderAlbums(sortByAlbumTitle);
 }
 
-
 const scoreButton = document.getElementById("sortByScore");
 const artistButton = document.getElementById("sortByArtist");
 const albumButton = document.getElementById("sortByAlbum");
+const applyFilters = document.getElementById("applyFilters");
 scoreButton.addEventListener("click", sortByScoreButton);
 artistButton.addEventListener("click", sortByArtistButton);
 albumButton.addEventListener("click", sortByAlbumButton);
+applyFilters.addEventListener("click", sortByScoreButton);
+
+let checkboxes = document.querySelectorAll("input[type=checkbox]");
+
+checkboxes.forEach(ele => {
+    ele.addEventListener('change', function () {
+        if (this.checked) {
+            console.log("Checkbox is checked..");
+            // remove from filters array
+            let genreToRemove = this.value;
+            let index = filters.indexOf(genreToRemove);
+            if (index != -1) {
+                filters.splice(index, 1);
+                console.log(filters);
+            }
+
+        } else {
+            console.log("Checkbox is unchecked..");
+            filters.push(this.value)
+            console.log(this.value);
+            console.log(filters);
+
+        }
+    });
+})
